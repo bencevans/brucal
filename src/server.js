c26afb382d241099b5250f007eb81962174bd537
Ben Evans <ben@bluechimp.io>
2017-09-26 21:35:46 +0100

    chore: remove remains of basic-auth

--- src/server.js
+++ src/server.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const express = require('express');
-const basicAuth = require('basic-auth');
 const {retreiveTimetable, buildIcal} = require('./');
 
 const app = express();
